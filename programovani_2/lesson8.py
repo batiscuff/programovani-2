c905e1c888814ce891c24468fe1de077ced4af05
batiscuff <batiscuff@protonmail.com>
2021-05-05 17:22:17 +0300

    Optimized

--- programovani_2/lesson8.py
+++ programovani_2/lesson8.py
@@ -5,11 +5,9 @@ def up_to_ten(number):
     return number * 10
 
 def my_big_map(word_list, func_list):
-    results = None
     for func in func_list:
-        elements_list = word_list if results is None else results
-        results = my_map(elements_list, func)
-    return results
+        word_list = my_map(word_list, func)
+    return word_list
 
 
 if __name__ == "__main__":
